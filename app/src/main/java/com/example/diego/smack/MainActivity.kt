e204783ab87b1278b7dd615d9774f52709da0534
diego <dcriado@humana.com>
2018-12-20 18:21:08 -0300

    lesson 3

--- app/src/main/java/com/example/diego/smack/MainActivity.kt
+++ app/src/main/java/com/example/diego/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diego.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -33,6 +34,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View){
+
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
